7974b98e660af0cbebb57d91404f5b1655f11578
Mario Nevado <nevado.mario@gmail.com>
2021-05-21 08:53:11 +0200

    helm3 install

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ RUN apt update
 
 RUN wget https://github.com/mikefarah/yq/releases/download/3.4.0/yq_linux_amd64 -O /usr/bin/yq &&    chmod +x /usr/bin/yq
 
+RUN wget https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz
+
+RUN tar zxf helm-v3.2.1-linux-amd64.tar.gz
+
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,24 +8,39 @@ pass=$4
 
 ls
 
+#------------------- CAT VERSION / CHART --------------------
+
 echo "version values: "
 cat values-files/version-values.yaml
 
 chartYaml=dgp-app/Chart.yaml
 cat $chartYaml
 
+#------------------- CHART REPLACE --------------------
+
 echo " ---- CHART REPLACE ----"
 yq w -i $chartYaml 'version' $tagNr
 yq w -i $chartYaml 'appVersion' $tagNr
 
+echo "cat new Chart.yml"
 cat $chartYaml
 
-# helm repo add nexusdgp https://admin:doG00d@nexus.dogoodpeople.net/repository/helm-dgp/
+#------------------- HELM 3 INSTALL -------------------
+ls linux-amd64
+cd linux-amd64
+mv helm /usr/local/bin/helm3
+cd ..
+helm3 version
+
+#------------------- HELM PACKAGE  --------------------
+
 PATH_OUT=charts-versions
 
 rm $PATH_OUT/*
 echo "deploying helm version $tagNr to path $PATH_OUT"
-helm package dgp-app/ --version $tagNr --destination $PATH_OUT
+helm3 package dgp-app/ --version $tagNr --destination $PATH_OUT
+
+#------------------- HELM DEPLOY --------------------
 
 FILE_CHART=$PATH_OUT/dgp-app-$tagNr.tgz
 echo "Uploading file $FILE_CHART to repo $repo "
